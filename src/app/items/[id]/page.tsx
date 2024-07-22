0b84861894bedd794b88cea57e4e1ef11db1907f
soojinpark99 <tnwls7263@gmail.com>
2024-07-22 12:56:29 +0900

    feat: 동적 경로 생성

--- src/app/items/[id]/page.tsx
+++ src/app/items/[id]/page.tsx
@@ -1,9 +1,22 @@
+import { getItems } from "@/lib/api";
 import styles from "@/styles/ItemDetail.module.css";
 
-export default function ItemDetail() {
+interface ItemsDetailParams {
+  params: { id: string };
+}
+
+export async function generateStaticParams() {
+  const items = await getItems();
+  return items.map((item) => ({
+    id: item.id.toString(),
+  }));
+}
+
+export default function ItemDetail({ params }: ItemsDetailParams) {
+  const { id } = params;
   return (
     <section className={styles.section}>
-      <div className={styles.itemName}>비타민 챙겨 먹기</div>
+      <div className={styles.itemName}>{id}비타민 챙겨 먹기</div>
     </section>
   );
 }
