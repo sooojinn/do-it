051eae4a9ad18f2ec60ba6d9fba9446be61f8e63
soojinpark99 <tnwls7263@gmail.com>
2024-07-23 15:41:37 +0900

    feat: 파일명 유효성 검사 추가

--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -4,7 +4,7 @@ import styles from "@/styles/FileInput.module.css";
 
 interface FileInputProp {
   name: string;
-  value: string | File | null; // value는 File 객체 또는 null이어야 합니다.
+  value: string | File | null;
   initialPreview: string;
   onChange: (name: string, value: string | File | null) => void;
 }
@@ -22,6 +22,17 @@ export default function FileInput({
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
+      // 파일 이름 유효성 검사
+      if (!/^[a-zA-Z0-9._-]+$/.test(file.name)) {
+        alert(
+          "파일 이름은 영어, 숫자, 점(.), 밑줄(_), 하이픈(-)만 포함해야 합니다."
+        );
+        if (inputRef.current) {
+          inputRef.current.value = "";
+        }
+        return;
+      }
+
       // 파일 크기 제한 (5MB 이하)
       if (file.size > 5 * 1024 * 1024) {
         alert("파일 크기는 5MB 이하만 가능합니다.");
@@ -31,6 +42,7 @@ export default function FileInput({
         return;
       }
 
+      // FileReader를 사용하여 파일을 base64 문자열로 변환
       const reader = new FileReader();
       reader.onloadend = () => {
         const base64String = reader.result as string;
@@ -42,23 +54,24 @@ export default function FileInput({
     }
   };
 
-  const handleClearClick = () => {
-    if (inputRef.current) {
-      inputRef.current.value = ""; // 파일 선택 초기화
-    }
-    onChange(name, null);
-  };
-
   useEffect(() => {
+    // value가 File 객체인 경우
     if (value instanceof File) {
+      // File 객체로부터 URL을 생성하여 이미지 미리보기 설정
       const nextPreview = URL.createObjectURL(value);
       setPreview(nextPreview);
 
       return () => {
         URL.revokeObjectURL(nextPreview);
       };
+
+      // value가 문자열(이미 URL이나 base64 형식)인 경우
+      // 해당 문자열을 그대로 미리보기로 설정
     } else if (typeof value === "string") {
       setPreview(value);
+
+      // value가 null이거나 undefined인 경우
+      // 초기 미리보기 이미지(initialPreview)를 사용
     } else {
       setPreview(initialPreview);
     }
@@ -74,14 +87,14 @@ export default function FileInput({
           alt="이미지 미리보기"
         />
         <label className={styles.fileSelectBtn} htmlFor="fileInput">
-          <button className={`${styles.btn} ${styles[type]}`}>
+          <div className={`${styles.btn} ${styles[type]}`}>
             <Image
               src={`/file_${type}.svg`}
               width={24}
               height={24}
               alt="수정"
             />
-          </button>
+          </div>
         </label>
       </div>
       <input
